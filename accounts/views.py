cc2b85230e94100c34700b97cb55fff407a48f52
adminstrator1999 <hopebegmuhammadov@gmail.com>
2021-04-05 22:55:44 +0500

    My second

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import  User
 
 def register(request):
@@ -41,8 +41,17 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        # login user
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('accounts:dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('accounts:login')
     else:
         return render(request, 'accounts/login.html')
 
