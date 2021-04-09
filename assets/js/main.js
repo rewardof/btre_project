160b29a9c443753b400f24ae5164ca1c5e832c27
adminstrator1999 <hopebegmuhammadov@gmail.com>
2021-04-09 21:56:21 +0500

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,2 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
-setTimeout(function () {
-    $('#message').fadeOut('slow');
-}, 3000)
--- btre/settings.py
+++ btre/settings.py
@@ -131,7 +131,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
-
 # media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
